46bbf80445ddd8353254e1a9ddfe83ed4e4dddb9
Grace Jeong <grace@Graceui-MacBookAir.local>
2024-04-17 22:01:51 +0200

    level2

--- tab.js
+++ tab.js
@@ -10,13 +10,35 @@
 
 //복붙이 될떄마다 eq() 가 0,1,2 되게끔하기
 
-for (let i = 0; i < $('.tab-button').length; i ++){
-    $('.tab-button').eq(i).on('click', function(){
-    $('.tab-button').removeClass('orange');
-    $('.tab-button').eq(i).addClass('orange');
-    $('.tab-content').removeClass('show');
-    $('.tab-content').eq(i).addClass('show');
-                    
-});
+// for (let i = 0; i < 3; i ++){
+//     $('.tab-button').eq(1).on('click', function(){
+//         탭열기(1)
+// })
     
+// }
+
+
+$('.list').click(function(e){
+
+    탭열기(e.target.dataset.id)
+    
+    //  탭열기(0)
+    // if(e.target == document.querySelectorAll('.tab-button')[0]){
+    //     탭열기(0)
+    // }
+    // // 지금누른게 버튼1이면 탭열기(0)
+    // if(e.target == document.querySelectorAll('.tab-button')[1]){
+    //     탭열기(1)
+    // }
+    // // 지금누른게 버튼2이면 탭열기(0)
+    // if(e.target == document.querySelectorAll('.tab-button')[2]){
+    //     탭열기(2)
+    // }
+});
+
+function 탭열기(숫자){
+    $('.tab-button').removeClass('orange');
+    $('.tab-button').eq(숫자).addClass('orange');
+    $('.tab-content').removeClass('show');
+    $('.tab-content').eq(숫자).addClass('show');
 }
